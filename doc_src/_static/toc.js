f1fd1437a8f0d85fb6c77c01ad6eb0f717a7ea0e
Adam Grant <adam.grant@sv.cmu.edu>
2011-02-12 21:59:11 -0800

    Initial commit following geocamDjangoAppSkeleton creation

---
+++ doc_src/_static/toc.js
@@ -0,0 +1,22 @@
+// __NO_RELICENSE__
+
+var TOC = {
+    load: function () {
+        $('#toc_button').click(TOC.toggle);
+    },
+    
+    toggle: function () {
+        if ($('#sphinxsidebar').toggle().is(':hidden')) {
+            $('div.document').css('left', "0px");
+            $('toc_button').removeClass("open");
+        } else {
+            $('div.document').css('left', "230px");
+            $('#toc_button').addClass("open");
+        }
+        return $('#sphinxsidebar');
+    }
+};
+
+$(document).ready(function () {
+    TOC.load();
+});
---
+++ geocamMemo/tests.py
@@ -0,0 +1,15 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from django.test import TestCase
+
+
+class geocamMemoTest(TestCase):
+    """
+    Tests for geocamMemoWeb
+    """
+    def test_geocamMemo(self):
+        pass
