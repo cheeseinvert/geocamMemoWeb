85fbc45f2456b4377ea3b2430eb9f4270b51d43e
Patrick Baumann <baumannpat@gmail.com>
2011-03-16 19:02:15 -0700

    Test of recipient model complete [#9480663]

--- geocamTalk/tests/unit.py
+++ geocamTalk/tests/unit.py
@@ -6,3 +6,25 @@
 
 from django.test import TestCase
 from django.contrib.auth.models import User
+
+class GeocamTalkUnitTest(TestCase):
+    fixtures = ['demoTalkMessages.json', 'demoUsers.json']
+    
+    def setUp(self):
+        self.now = datetime.now()
+    
+    def testEnsureRecipientsCanBeAddedToAMessage(self):
+        # arrange
+        sender = User.objects.all()[0]
+        recipienta = User.objects.all()[1]
+        recipientb = User.objects.all()[2]
+
+        # act
+        message = MemoMessage.objects.create(
+            content="012345678901234567890123456789", 
+            content_timestamp=self.now, 
+            author=sender, 
+            recipients = [recipienta, recipientb])
+        
+        # assert
+        self.assertEquals(2, len(message.recipients), "All recipients should be added to the message")
