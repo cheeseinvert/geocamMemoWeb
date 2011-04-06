589bc080bf5ff41c06c505470753ff5dce6e258f
Sam Karp <sam.karp@sv.cmu.edu>
2011-04-05 22:47:23 -0600

    Testing filename is accurate

--- example/settings.py
+++ example/settings.py
@@ -45,17 +45,17 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.abspath(os.path.join('media'))
+#MEDIA_ROOT = os.path.abspath(os.path.join('media'))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/static/'
+#MEDIA_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+#ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'g2_39yupn*6j4p*cg2%w643jiq-1n_annua*%i8+rq0dx9p=$n'
--- geocamTalk/tests/unit.py
+++ geocamTalk/tests/unit.py
@@ -8,6 +8,9 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from datetime import datetime
 from geocamTalk.models import TalkMessage
+from django.core.files.base import ContentFile
+import array, os, random
+
 
 class GeocamTalkUnitTest(TestCase):
     fixtures = ['demoTalkMessages.json', 'demoUsers.json']
@@ -86,3 +89,52 @@ class TalkUserProfileUnitTest(TestCase):
                 
         # assert
         self.assertEquals(currentCount + 1, profile.getUnreadMessageCount())
+
+    def testEnsureFromJsonCreatesMessage(self):        
+        #arrange
+        timestamp = datetime(2011, 04, 03, 14, 30, 00)
+        
+        message = dict(                    
+                    userId=User.objects.all()[0].pk,
+                    content="Sting!!!",
+                    contentTimestamp=timestamp.strftime("%m/%d/%y %H:%M:%S"),
+                    latitude=1.1,
+                    longitude=222.2,
+                    accuracy=60 )
+        audioFile = 'test_ensure_from_json.mp4'
+        self._createFile(filename=audioFile, filesize=100*1024)
+        f = open(audioFile, "rb")
+        #act
+        talkMessage = TalkMessage.fromJson(message)
+        talkMessage.audio_file.save(f.name, ContentFile(f.read()))
+        talkMessage.save()
+            
+        #assert
+        self.assertEqual(talkMessage.author.pk, User.objects.all()[0].pk)
+        self.assertEqual(talkMessage.content, "Sting!!!")
+        self.assertEqual(talkMessage.content_timestamp, timestamp)
+        self.assertEqual(talkMessage.latitude, 1.1)
+        self.assertEqual(talkMessage.longitude, 222.2)
+        self.assertEqual(talkMessage.accuracy, 60)
+        self.assertTrue(talkMessage.has_audio())
+        self.assertEqual(os.path.basename(talkMessage.audio_file.name), f.name)
+        f.close() 
+        os.remove(os.path._getfullpathname(talkMessage.audio_file.name))
+        
+    def _createFile(self, filename, filesize=5*1024*1024):
+        """Create and fill a file with random data"""
+        blocksize = 4096 # 4k
+        datablock = array.array('I')
+        written = 0
+        # Create a datablock:
+        while written < blocksize:
+            datablock.append(random.getrandbits(32))
+            written = written + 4
+            
+        with open(filename, 'w') as f:
+            written = 0
+            while written < filesize:
+                datablock.tofile(f)
+                written += blocksize
+            f.flush()
+            os.fsync(f.fileno())
